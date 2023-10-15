a42f7b8ee8dfd992e7f5e5c902414ee128697723
Arnab Dey <arnabdey710@gmail.com>
2023-10-15 22:26:07 +0530

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { Toaster } from "react-hot-toast";
-import Image from "next/image";
-import bg from "../../public/assets/background.jpg";
+// import Image from "next/image";
+// import bg from "../../public/assets/background.jpg";
 
 const poppins = Poppins({
 	weight: ["300", "400", "500", "600", "700", "800", "900"],
@@ -25,7 +25,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 	return (
 		<html lang="en">
 			<body className={`${poppins.className} relative w-[full]  overflow-x-hidden bg-black text-white`}>
-				<Image src={bg} priority alt="my-background" style={{ position: `fixed`, top: 0, left: 0, width: `100%`, height: `100vh`, zIndex: -99 }} />
+				{/* <Image src={bg} priority alt="my-background" style={{ position: `fixed`, top: 0, left: 0, width: `100%`, height: `100vh`, zIndex: -99 }} /> */}
 				{children}
 				<Toaster
 					position="bottom-center"
