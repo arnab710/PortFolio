7ceab313d3d0a55b8560343a8f1b6eeea4c16530
Arnab Dey <arnabdey710@gmail.com>
2023-10-15 22:05:06 +0530

    removed unused configs in tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,49 +17,19 @@ const config: Config = {
 				tremor: {
 					brand: {
 						faint: "#eff6ff", // blue-50
-						muted: "#bfdbfe", // blue-200
-						subtle: "#60a5fa", // blue-400
 						DEFAULT: "#3b82f6", // blue-500
-						emphasis: "#1d4ed8", // blue-700
-						inverted: "#ffffff", // white
-					},
-					background: {
-						muted: "#f9fafb", // gray-50
-						subtle: "#f3f4f6", // gray-100
-						DEFAULT: "#ffffff", // white
-						emphasis: "#374151", // gray-700
-					},
-					border: {
-						DEFAULT: "#e5e7eb", // gray-200
-					},
-					ring: {
-						DEFAULT: "#e5e7eb", // gray-200
+						muted: "#bfdbfe",
 					},
 					content: {
-						subtle: "#9ca3af", // gray-400
-						DEFAULT: "#6b7280", // gray-500
 						emphasis: "#fff",
-						strong: "#111827", // gray-900
-						inverted: "#ffffff", // white
 					},
 				},
 			},
-			boxShadow: {
-				// light
-				"tremor-input": "0 1px 2px 0 rgb(0 0 0 / 0.05)",
-				"tremor-card": "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
-				"tremor-dropdown": "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
-			},
 			borderRadius: {
-				"tremor-small": "0.375rem",
-				"tremor-default": "0.5rem",
 				"tremor-full": "9999px",
 			},
 			fontSize: {
-				"tremor-label": ["0.75rem", { lineHeight: "1rem" }],
 				"tremor-default": ["1rem", { lineHeight: "1.25rem" }],
-				"tremor-title": ["1.125rem", { lineHeight: "1.75rem" }],
-				"tremor-metric": ["1.875rem", { lineHeight: "2.25rem" }],
 			},
 			keyframes: {
 				waveAnimation: {
