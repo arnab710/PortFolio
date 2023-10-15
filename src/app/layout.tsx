fe12d8cda4dc3dd116381ec8c3cd633d899853f4
Arnab Dey <arnabdey710@gmail.com>
2023-10-15 22:22:22 +0530

    customisation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,9 +23,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en" className="relative">
-			<body className={`${poppins.className} w-[full] overflow-x-hidden  bg-black text-white`}>
-				<Image src={bg} priority alt="my-background" style={{ position: `fixed`, top: 0, left: 0, width: `100%`, height: `auto`, zIndex: -99 }} />
+		<html lang="en">
+			<body className={`${poppins.className} relative w-[full]  overflow-x-hidden bg-black text-white`}>
+				<Image src={bg} priority alt="my-background" style={{ position: `fixed`, top: 0, left: 0, width: `100%`, height: `100vh`, zIndex: -99 }} />
 				{children}
 				<Toaster
 					position="bottom-center"
